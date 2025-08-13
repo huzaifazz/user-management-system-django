8094403201c036c77c4c0faf3ae206d39d2c2d6f
Huzaifa Asad <khuzaifaasad55@gmail.com>
2025-08-13 11:21:46 +0500

    Role based feature changed by admin only.

--- website/main/urls.py
+++ website/main/urls.py
@@ -9,6 +9,10 @@ urlpatterns = [
     path('create-post', views.create_post, name='create_post'),
     path('delete_post/<int:post_id>/', views.delete_post, name='delete_post'),
     path('logout/', views.logout_view, name='logout'),
-        path('my-comments/', views.superuser_comments, name='superuser_comments'),
-        path('delete_comment/<int:comment_id>/', views.delete_comment, name='delete_comment'),
+    path('my-comments/', views.superuser_comments, name='superuser_comments'),
+    path('delete_comment/<int:comment_id>/', views.delete_comment, name='delete_comment'),
+    path('profile/', views.view_profile, name='view_profile'),
+    path('profile/edit/', views.edit_profile, name='edit_profile'),
+    path('profile/edit/<str:username>/', views.admin_edit_profile, name='admin_edit_profile'),
+    path('profile/<str:username>/', views.view_profile, name='view_profile_other'),
 ]

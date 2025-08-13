7b406f5bac77dc313ce2000edd78764eb1edb6b2
Huzaifa Asad <khuzaifaasad55@gmail.com>
2025-08-13 11:18:48 +0500

    Add profile form new feature

--- website/main/forms.py
+++ website/main/forms.py
@@ -1,8 +1,14 @@
+# Profile form
+from .models import Profile
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Post
 
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ["avatar", "bio", "website", "twitter", "github", "role"]
 class RegisterForm(UserCreationForm):
     email = forms.EmailField(required=True)
     
